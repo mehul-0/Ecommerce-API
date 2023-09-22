76734d427aa9c074ecfba2f4560155184f8a2c10
Mehul <mehulgidwani03@gmail.com>
2023-09-22 17:35:22 +0530

    first commit

--- models/product.js
+++ models/product.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 // creating the product schema
 const productSchema = new mongoose.Schema({
-
     name: String,
     quantity: Number
-},{
-    versionKey: false
 });
 
 // creating a new model called "Product"
